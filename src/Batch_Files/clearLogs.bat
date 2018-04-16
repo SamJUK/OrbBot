4894ee810e2a54e8d0c2fbab4474fddd63f1e63c
Sam James <sam@samdjames.uk>
2018-04-16 23:25:15 +0100

    Repo Sort

--- src/Batch_Files/clearLogs.bat
+++
@@ -1,2 +0,0 @@
-cd ../logs/
-del *.log
--- src/Batch_Files/install.bat
+++
@@ -1,8 +0,0 @@
-REM npm install --save discord.js
-REM npm install --save request
-npm install .
-
-REM Create Credential Files
-cd ../Credentials
-echo. 2>discord_bot.token
-echo. 2>pubg_api.key
